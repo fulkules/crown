d117771a197427a9876191f972f2ab1849fbd6e8
Barry Fulk <barry.fulk@churchofjesuschrist.org>
2021-08-12 16:33:21 -0600

    remove injected import

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react';
 
 const config = {
     apiKey: "AIzaSyBv11-VzaHu7lR4r45WOJRa45QOTIZN1V4",
