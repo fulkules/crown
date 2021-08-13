83ac17d2dce667f9d3e07698d99d11f10f365fcf
Barry Fulk <barry.fulk@churchofjesuschrist.org>
2021-08-13 08:28:49 -0600

    complete sign-in and sign-up functionality

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ class App extends Component {
                 id: snapShot.id,
                 ...snapShot.data()
               }
-            });
+            }, () => console.log(this.state));
           });
         } else {
           this.setState({ currentUser: userAuth });
         }
-      })
+      });
     }
 
     componentWillUnmount() {
