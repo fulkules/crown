ad196eaf17d858ea459631119cb1924f91f506c8
Barry Fulk <barry.fulk@churchofjesuschrist.org>
2021-08-26 08:58:08 -0600

    collections overview and nested routing for collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
+const ShopPage = ({ match }) => {
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+    return(
+        <div className="shop-page">
+            <Route exact path={`${match.path}`} component={ CollectionsOverview } />
+            <Route path={`${match.path}/:collectionId`} component={ CollectionPage } />
+        </div>
+    );
+};
 
-    render(){
-        const { collections } = this.state;
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} { ...otherCollectionProps } />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+
 
 export default ShopPage;
